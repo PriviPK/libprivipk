10c9cbc359b2ece56b6b13df11d5996e3f844b36
Johnny C. Encrypt <johnny.can.encrypt@gmail.com>
2015-12-05 16:41:15 -0500

    oops, forogot to check in README changes and Vagrantfile

---
+++ install-vagrant.sh
@@ -0,0 +1,3 @@
+sudo apt-get install vagrant virtualbox cpu-checker
+
+vagrant box add ubuntu/trusty64 https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box
